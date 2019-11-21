19feb148444172eb1d65dcea7ab96aa02a2ee409
Stewart Granger Flores <stewart.grngrf@gmail.com>
2019-11-20 22:41:36 -0300

    Updates

---
+++ precache-manifest.66442745dbe2355b2b8613a004ee4869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a9fc68b8bdec04b4291a69f6749fd9",
+    "url": "/Clima-App-React/index.html"
+  },
+  {
+    "revision": "09635315d6b8d09a1bf3",
+    "url": "/Clima-App-React/static/css/main.ee01c68b.chunk.css"
+  },
+  {
+    "revision": "8ffeed16ef93f0068319",
+    "url": "/Clima-App-React/static/js/2.cca50120.chunk.js"
+  },
+  {
+    "revision": "09635315d6b8d09a1bf3",
+    "url": "/Clima-App-React/static/js/main.b6d4c5bc.chunk.js"
+  },
+  {
+    "revision": "02c723c58a2306b0c291",
+    "url": "/Clima-App-React/static/js/runtime-main.818d0c16.js"
+  }
+]);
